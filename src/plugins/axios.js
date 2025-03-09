c02263d68f4086d652ca57968cd555f0aae231d6
AbdulghaniKM <abdulghanim70@gmail.com>
2025-03-09 15:29:45 +0300

    refactor: :recycle: removed annoying console.logs

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
-
-// API Configuration
 export const API_CONFIG = {
   BASE_URL: 'https://pmsapi.alfakharco.com/',
 };
-
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://pmsapi.alfakharco.com/',
   timeout: 10000,
@@ -13,22 +10,16 @@ const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-// Add request interceptor for debugging
 axiosInstance.interceptors.request.use(
   (config) => {
-    console.log('Request:', config.method.toUpperCase(), config.url);
     return config;
   },
   (error) => {
     return Promise.reject(error);
   }
 );
-
-// Add response interceptor for debugging
 axiosInstance.interceptors.response.use(
   (response) => {
-    console.log('Response:', response.status, response.data);
     return response;
   },
   (error) => {
@@ -40,5 +31,4 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
 export default axiosInstance;
