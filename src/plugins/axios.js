3ea0da7d5000435ae5413666ed0ef21687b2c80a
ahmed <ahmed6ali404@gmail.com>
2025-03-10 13:55:59 +0300

    feat: Integrate users API and update user details page
    fetching user details now works and displays user info in GET

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
+import { useAuthStore } from '@/stores/authStore';
+
 export const API_CONFIG = {
   BASE_URL: 'https://pmsapi.alfakharco.com/',
 };
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://pmsapi.alfakharco.com/',
   timeout: 10000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
+    'Accept': 'application/json',
   },
 });
+
 axiosInstance.interceptors.request.use(
   (config) => {
+    const authStore = useAuthStore();
+    if (authStore.token) {
+      config.headers.Authorization = `Bearer ${authStore.token}`;
+    }
     return config;
   },
   (error) => {
