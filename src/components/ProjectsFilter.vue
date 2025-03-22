5f600cb5a05fce7b0c0357890a8deacf07859a9b
ahmed <ahmed6ali404@gmail.com>
2025-03-22 12:40:34 +0300

    feat: :sparkles: update ProjectsFilter to include regional funding option and adjust layout for Slider

--- src/components/ProjectsFilter.vue
+++ src/components/ProjectsFilter.vue
@@ -83,9 +83,9 @@
               :class="{ 'cursor-not-allowed': disabled }"
             />
             <CustomCheckbox
-              v-model="localSelectedFunding.fund"
-              id="fund"
-              label="ممولة"
+              v-model="localSelectedFunding.regional"
+              id="regional"
+              label="تنمية الاقاليم"
               :disabled="disabled"
               :class="{ 'cursor-not-allowed': disabled }"
             />
@@ -119,8 +119,9 @@
               :disabled="!isBudgetFilterEnabled || disabled"
               :class="{ 'cursor-not-allowed': disabled }"
               @update:model-value="handleSliderChange"
+              dir="rtl"
             />
-            <div class="grid grid-cols-2 gap-4">
+            <div class="grid grid-cols-2 gap-4 flex-row-reverse">
               <div class="space-y-2">
                 <label class="text-xs text-gray-500 dark:text-gray-400">الحد الادنى</label>
                 <NumberInput
@@ -365,6 +366,7 @@
     investment: false,
     operational: false,
     environment: false,
+    regional: false,
   });
 
   // Add a new ref for budget filter enablement
